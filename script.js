aa37b0ee562840e3cdbbac9ea5472583d288a626
Saule <saulerickwood25@gmail.com>
2025-04-11 14:03:39 +0100

    finished hw

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function gameOne() {
-    
+document.getElementById('play-button').addEventListener('click',
+    function gameOne() {
+        let randomNumber = +prompt('Угадай число');
+        if (randomNumber < 0) {
+            alert('число не может быть отрицательным');
+        } else if (randomNumber >= 0 && randomNumber <= 100) {
+            alert('молодец,угадал!')
+        } else if (randomNumber > 100) {
+            alert('число не может быть больше 100!');
+        } else {
+            alert('Вы ввели неправильное значениие!');
+        }
 
-    
-}
+        console.log('Кнопка "Играть" нажата!');
+    });
