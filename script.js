67055b9a756e116c12b7510156720a5e354c6fe7
Saule <saulerickwood25@gmail.com>
2025-04-22 17:12:42 +0100

    game 5

--- script.js
+++ script.js
@@ -140,3 +140,46 @@ function quiz() {
   }
  alert(`Вы правильно ответили на ${correctAnswers} вопроса!`) 
 }
+
+
+function rockScissorsPaper() {
+  const userAnswer = prompt('Введите вариант: Камень ножницы бумага!')
+  const options = ['камень', 'ножницы', 'бумага']
+  const compAnswer = options[Math.floor(Math.random() * options.length)];
+  if (!userAnswer) {
+    alert('Игра отменена!')
+    return
+    
+  }
+  if (userAnswer.toLowerCase() === compAnswer.toLowerCase()) {
+    alert('Ничья');
+  }
+  else {
+    if (userAnswer.toLowerCase() === options[0] && compAnswer.toLowerCase() === options[1]) {
+      alert('Вы выйграли!');
+    }
+
+    else if (userAnswer.toLowerCase() === options[0] && compAnswer.toLowerCase() === options[2]) {
+      alert('Вы проиграли!');
+    }
+
+     else if (userAnswer.toLowerCase() === options[1] && compAnswer.toLowerCase() === options[0]) {
+      alert('Вы проиграли!');
+    } 
+    else if (userAnswer.toLowerCase() === options[1] && compAnswer.toLowerCase() === options[2]) {
+      alert('Вы выйграли!');
+
+    } 
+    else if (userAnswer.toLowerCase() === options[2] && compAnswer.toLowerCase() === options[0]) {
+      alert('Вы выйграли!');
+    } 
+    else if (userAnswer.toLowerCase() === options[2] && compAnswer.toLowerCase() === options[1]) {
+      alert('Вы проиграли!');
+    } 
+    else {
+      alert('Вы ввели что-то не то!')
+    }
+  }
+  console.log(compAnswer);
+}
+
